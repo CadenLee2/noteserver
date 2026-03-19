db19c3f5fb236268b55fef2031d99b02f75b7038
Cadecraft <cadendotlee@gmail.com>
2026-03-19 13:43:12 -0700

    feat: markdown rendering

--- src/rendering.rs
+++ src/rendering.rs
@@ -1,14 +1,65 @@
+const STYLE_RULES: &str = r#"
+body {
+    font-size: 16px;
+    background-color: #e3e3e5;
+    color: #1e2128;
+    font-family: 'Lora', 'Arial';
+    display: flex;
+    justify-content: center;
+    padding: 4px 16px;
+}
+
+body > div {
+    width: 100%;
+    max-width: 820px;
+}
+
+h1 {
+    font-size: 26px;
+}
+
+h2 {
+    font-size: 22px;
+}
+
+h3 {
+    font-size: 18px;
+}
+
+h1, h2, h3 {
+    margin-top: 22px;
+    margin-bottom: 4px;
+}
+
+p {
+    margin-block: 12px;
+}
+
+a, a:visited {
+    color: #1165aa;
+}
+
+a:hover {
+    color: #3f87c1;
+    cursor: pointer;
+}
+
+hr {
+    border: solid 1px #babdc1;
+    margin-top: 40px;
+}
+"#;
+
 fn front_matter(title: &str) -> String {
     format!(r#"
 <head>
-    <title>{}</title>
-    <style>
-        h1 {{
-            font-size: 18px;
-        }}
-    </style>
+<title>{}</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lora">
+<style>
+{}
+</style>
 </head>
-"#, title)
+"#, title, STYLE_RULES)
 }
 
 pub fn error_page(error: &str) -> String {
@@ -25,15 +76,38 @@ pub fn error_page(error: &str) -> String {
 }
 
 pub fn directory(dir: &str, note_titles: &Vec<String>, description: &Option<String>) -> String {
-    let note_list = note_titles
-        .iter()
-        .map(|n| format!("<li>{}</li>", n))
-        .collect::<String>();
-
     let dir_descr_elem = match description {
         Some(d) => format!("<p>{}</p>", d),
         None => String::new(),
     };
+
+    const MISC_DIR_ID: &str = "misc";
+
+    if dir == MISC_DIR_ID {
+        return format!(
+            r#"
+<!DOCTYPE html>
+<html>
+{}
+<body>
+    <div>
+        <h1>Notes directory: {}</h1>
+        {}
+        <p>
+            <i>Cannot view all notes in this directory</i>
+        </p>
+    </div>
+</body>
+</html>
+"#,
+            front_matter(dir), dir, dir_descr_elem
+        );
+    }
+
+    let note_list = note_titles
+        .iter()
+        .map(|n| format!("<li><a href=\"/{}/{}\">{}</a></li>", dir, n, n))
+        .collect::<String>();
 
     format!(
         r#"
@@ -41,11 +115,13 @@ pub fn directory(dir: &str, note_titles: &Vec<String>, description: &Option<Stri
 <html>
 {}
 <body>
-    <h1>Notes directory: {}</h1>
-    {}
-    <ul>
-    {}
-    </ul>
+    <div>
+        <h1>Notes directory: {}</h1>
+        {}
+        <ul>
+        {}
+        </ul>
+    </div>
 </body>
 </html>
 "#,
@@ -54,8 +130,9 @@ pub fn directory(dir: &str, note_titles: &Vec<String>, description: &Option<Stri
 }
 
 pub fn note(dir: &str, note: &str, md_contents: &str) -> String {
-    // TODO: convert
-    let md_as_html = md_contents;
+    let md_as_html = markdown::to_html(md_contents);
+
+    let note_title = format!("{} ({})", note, dir);
 
     format!(
         r#"
@@ -63,12 +140,14 @@ pub fn note(dir: &str, note: &str, md_contents: &str) -> String {
 <html>
 {}
 <body>
-    {}
-    <hr>
-    <a href="/{}">Return to directory {}</a>
+    <div>
+        {}
+        <hr id="end">
+        <a id="return-to-dir" href="/{}">Return to directory {}</a>
+    </div>
 </body>
 </html>
 "#,
-        front_matter(note), md_as_html, dir, dir
+        front_matter(&note_title), md_as_html, dir, dir
     )
 }
