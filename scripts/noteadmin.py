2cc6370090eab58d53607217461e2b5b9a38e409
Cadecraft <cadendotlee@gmail.com>
2026-03-21 09:05:35 -0700

    feat: admin api url selection

--- scripts/noteadmin.py
+++ scripts/noteadmin.py
@@ -3,7 +3,8 @@
 import requests
 import os
 
-NOTESERVER_URL = 'http://localhost:3002'
+LOCAL_URL = 'http://localhost:3002'
+PROD_URL = 'https://notes.cadenlee.dev'
 
 # Helpers
 def help():
@@ -48,13 +49,19 @@ def post_bulk(remote_dir, local_folder):
     for entry in to_post:
         md_content = read_file(entry[1])
         print('Posting ' + entry[1])
-        resp = requests.post(NOTESERVER_URL + '/' + remote_dir + '/' + entry[0], md_content, headers=auth_headers)
+        resp = requests.post(api_url + '/' + remote_dir + '/' + entry[0], md_content, headers=auth_headers)
         print('Server returned ' + str(resp.status_code))
 
 # Start input
 print('noteserver admin')
 print()
 
+print("Shortcuts: 'l' for " + LOCAL_URL + ", 'p' for " + PROD_URL)
+api_url = input("Connection url: ")
+if api_url == 'l':
+    api_url = LOCAL_URL
+elif api_url == 'p':
+    api_url = PROD_URL
 password = input('Enter your admin password: ')
 print()
 
@@ -72,19 +79,19 @@ while True:
         elif (args[0] == 'help'):
             help()
         elif (args[0] == 'ls'):
-            resp = requests.get(NOTESERVER_URL + '/all', headers=auth_headers)
+            resp = requests.get(api_url + '/all', headers=auth_headers)
             print(resp.text)
         elif (args[0] == 'post'):
             remote_note = args[1]
             local_file = args[2]
             md_contents = read_file(local_file)
-            resp = requests.post(NOTESERVER_URL + '/' + remote_note, md_contents, headers=auth_headers)
+            resp = requests.post(api_url + '/' + remote_note, md_contents, headers=auth_headers)
             print(resp.status_code)
         elif (args[0] == 'post-dir'):
             remote_dir = args[1]
             protected = input('Protected? y/N: ') == 'y'
             descr = input('Description: ')
-            resp = requests.post(NOTESERVER_URL + '/' + remote_dir + '?protected=' + ('true' if protected else 'false'), descr, headers=auth_headers)
+            resp = requests.post(api_url + '/' + remote_dir + '?protected=' + ('true' if protected else 'false'), descr, headers=auth_headers)
             print(resp.status_code)
         elif (args[0] == 'post-bulk'):
             remote_dir = args[1]
@@ -92,20 +99,20 @@ while True:
             post_bulk(remote_dir, local_folder)
         elif (args[0] == 'delete!'):
             remote_note = args[1]
-            resp = requests.delete(NOTESERVER_URL + '/' + remote_note, headers=auth_headers)
+            resp = requests.delete(api_url + '/' + remote_note, headers=auth_headers)
             print(resp.status_code)
         elif (args[0] == 'delete-dir!'):
             remote_dir = args[1]
-            resp = requests.delete(NOTESERVER_URL + '/' + remote_dir, headers=auth_headers)
+            resp = requests.delete(api_url + '/' + remote_dir, headers=auth_headers)
             print(resp.status_code)
         elif (args[0] == 'post-tok'):
             remote_dir = args[1]
             tok = input('Enter token: ')
-            resp = requests.post(NOTESERVER_URL + '/token/' + tok + '?directory=' + remote_dir, headers=auth_headers)
+            resp = requests.post(api_url + '/token/' + tok + '?directory=' + remote_dir, headers=auth_headers)
             print(resp.status_code)
         elif (args[0] == 'delete-tok!'):
             tok = args[1]
-            resp = requests.delete(NOTESERVER_URL + '/token/' + tok, headers=auth_headers)
+            resp = requests.delete(api_url + '/token/' + tok, headers=auth_headers)
             print(resp.status_code)
         else:
             print('Invalid command')
